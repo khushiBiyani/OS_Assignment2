8375bb93223d181fc0a3a607ab28234edf67ecd5
Tushar Chenan <tusky2002@gmail.com>
2022-11-19 17:48:46 +0530

    Fixed main to incorporate PR #1, shared memory is fixed size and only part left is thread runner function

--- p1.c
+++ p1.c
@@ -9,23 +9,18 @@
 
 enum {
     MAXN = 5,          // maximum dimension of any matrix
-    MAXTHREADS = 1,    // maximum number of threads
+    MAXTHREADS = 2,    // maximum number of threads
 };
 
-int i;    // Row size of matrix in in1.txt
-int j;    // Col size of matrix in in1.txt = Row size of matrix in in2.txt
-int k;    // Col size of matrix in in2.txt
+int I;    // Row size of matrix in in1.txt
+int J;    // Col size of matrix in in1.txt = Row size of matrix in in2.txt
+int K;    // Col size of matrix in in2.txt
 
 char* inputFileOne;    // contains name of in1.txt
 char* inputFileTwo;    // contains name of in2.txt
 char* outputFile;
 
 pthread_t threadID[MAXTHREADS];    // stores ThreadIDs of MAXTHREADS threads
-int threadInfo[MAXTHREADS][3];     // stores information to be passed to each thread
-
-int currentRow;    // stores row to be read from fileOne
-int currentCol;    // stores column to be read from fileTwo
-int parity;        // if parity == 0 -> read row from fileOne, else read column from fileTwo
 
 // END: Global Variables
 
@@ -34,8 +29,11 @@ int parity;        // if parity == 0 -> read row from fileOne, else read column 
 int SHM_KEY;
 
 struct shmseg {
-    int visitedRow[MAXN];         // marks rows of matrix One as read from in1.txt
-    int visitedCol[MAXN];         // marks columns of matrix Two as read from in2.txt
+    int I;                        // rows in matrixOne
+    int J;                        // columns in matrixOne == rows in matrixTwo
+    int K;                        // columns in matrixTwo
+    int visitedRowOne[MAXN];      // marks rows of matrixOne as read from in1.txt
+    int visitedRowTwo[MAXN];      // marks rows of matrixTwo as read from in2.txt
     int matrixOne[MAXN][MAXN];    // stores matrix read from in1.txt
     int matrixTwo[MAXN][MAXN];    // stores matrix read from in2.txt
     char outputFile[505];
@@ -50,103 +48,112 @@ struct shmseg* shmp;    // stores pointer to shared memory
 
 void readRowsFromFileOne(int R) {
     // Reads row R of matrix in in1.txt into matrixOne[R][]
+    FILE* ptr;
+    ptr = fopen(inputFileOne, "r");
+
+    if (ptr == NULL) {
+        printf("Error opening %s\n", inputFileOne);
+        exit(-1);
+    }
+
+    int rowCnt = 0;
+    char* line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    while (rowCnt != R) {
+        read = getline(&line, &len, ptr);
+        rowCnt++;
+    }
+
+    int num;
+    for (int i = 0; i < J; i++) {
+        fscanf(ptr, "%d", &num);
+        shmp->matrixOne[R][i] = num;
+    }
+    shmp->visitedRowOne[R] = 1;
 }
 
 // END: Read rows from file in1.txt
 
-// BEGIN: Read columns from file in2.txt
+// BEGIN: Read rows from file in2.txt
 
-void readColumnsFromFileTwo(int C) {
-    // Reads column C of matrix in in2.txt into matrixTwo[][C]
+void readRowsFromFileTwo(int R) {
+    // Reads row R of matrix in in2.txt into matrixTwo[R][]
+    FILE* ptr;
+    ptr = fopen(inputFileTwo, "r");
+
+    if (ptr == NULL) {
+        printf("Error opening %s\n", inputFileTwo);
+        exit(-1);
+    }
+
+    int rowCnt = 0;
+    char* line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    while (rowCnt != R) {
+        read = getline(&line, &len, ptr);
+        rowCnt++;
+    }
+
+    int num;
+    for (int i = 0; i < K; i++) {
+        fscanf(ptr, "%d", &num);
+        shmp->matrixTwo[R][i] = num;
+    }
+    shmp->visitedRowTwo[R] = 1;
 }
 
-// END: Read columns from file in2.txt
+// END: Read rows from file in2.txt
 
 // BEGIN: Common Thread Runner Function
 
 void* runner(void* arg) {
-    // Common runner function for (i+k) threads
-    int t = *((int*) arg);
-
-    int currentRow = threadInfo[t][0];
-    int currentCol = threadInfo[t][1];
-    int parity = threadInfo[t][2];
-    pthread_t tid = pthread_self();
-
-    if (parity) {
-        printf("[%lu] Reading Column: %d\n", tid, currentCol);
-    } else {
-        printf("[%lu] Reading Row: %d\n", tid, currentRow);
-    }
+    // Common runner function for MAXTHREADS threads
+    int id = (int) arg;
 
     pthread_exit(NULL);
 }
 
 // END: Common Thread Runner Function
 
-// BEGIN: Find next free thread
-
-int findNextFreeThread() {
-    // returns index of next free thread from [0, MAXTHREADS) using threadID array
-    sleep(1);
-    return 0;
-}
-
-// END: Find next free thread
-
 int main(int argc, char* argv[]) {
     if (argc != 7) {
         printf("Usage: ./p1 i j k in1.txt in2.txt out.txt\n");
         exit(-1);
     }
-    i = atoi(argv[1]);
-    j = atoi(argv[2]);
-    k = atoi(argv[3]);
+    I = atoi(argv[1]);
+    J = atoi(argv[2]);
+    K = atoi(argv[3]);
     inputFileOne = argv[4];
     inputFileTwo = argv[5];
     outputFile = argv[6];
 
     SHM_KEY = ftok("./p1.c", 0x2);
-
     shmid = shmget(SHM_KEY, sizeof(struct shmseg), 0644 | IPC_CREAT);
     if (shmid == -1) {
         perror("Shared memory");
         exit(-1);
     }
-
     shmp = shmat(shmid, NULL, 0);
     if (shmp == (void*) -1) {
         perror("Shared memory attach");
         exit(-1);
     }
 
+    shmp->I = I;
+    shmp->J = J;
+    shmp->K = K;
+    memset(shmp->visitedRowOne, 0, sizeof(shmp->visitedRowOne));
+    memset(shmp->visitedRowTwo, 0, sizeof(shmp->visitedRowTwo));
     strcpy(shmp->outputFile, outputFile);
 
-    for (int z = 0; z < i + k; ++z) {
-        int pos;
-        do {
-            pos = findNextFreeThread();
-        } while (pos == -1);
-        threadInfo[pos][0] = currentRow;
-        threadInfo[pos][1] = currentCol;
-        threadInfo[pos][2] = parity;
-        if (currentRow < i && currentCol < j) {
-            if (parity) {
-                ++currentCol;
-            } else {
-                ++currentRow;
-            }
-            parity ^= 1;
-        } else if (currentRow < i) {
-            ++currentRow;
-        } else {
-            ++currentCol;
-        }
-        pthread_create(&threadID[pos], NULL, runner, (void*) &pos);
+    for (int i = 0; i < MAXTHREADS; ++i) {
+        pthread_create(&threadID[i], NULL, runner, (void*) i);
     }
 
-    for (int z = 0; z < MAXTHREADS; ++z) {
-        pthread_join(threadID[z], NULL);
+    for (int i = 0; i < MAXTHREADS; ++i) {
+        pthread_join(threadID[i], NULL);
     }
 
     if (shmdt(shmp) == -1) {
